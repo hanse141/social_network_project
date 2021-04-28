17387b3358cb6f6ebfc00d9b68c27342daa194ff
Alex Wang <71106044+Cha1rmanMe0w@users.noreply.github.com>
2021-04-28 12:19:18 -0400

    Add files via upload

---
+++ InvalidEmailException.java
@@ -0,0 +1,11 @@
+public class InvalidEmailException extends Exception {
+
+    public InvalidEmailException() {
+        super();
+    }
+
+    public InvalidEmailException(String message) {
+        super(message);
+    }
+
+}
